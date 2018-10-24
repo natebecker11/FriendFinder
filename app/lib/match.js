577286cbb2d36b6579884beaa3c478a8286a8af3
natebecker11 <natebecker11@gmail.com>
2018-10-24 19:19:14 -0400

    finished comparison/matching functions

--- app/lib/match.js
+++ app/lib/match.js
@@ -1,6 +1,30 @@
 
-const a = [1,1,1,1,1,1,1,1,1,1]
-const b = [3,2,3,5,5,5,1,2,3,5]
+// const a = [1,1,1,1,1,1,1,1,1,1]
+// const b = [3,2,3,5,5,5,1,2,3,5]
+
+// const friends = [
+//   {
+//     "name": "Joe",
+//     "photo": "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+//     "scores": [
+//       5, 4, 5, 4, 1, 2, 1, 2, 3, 1
+//     ]
+//   },
+//   {
+//     "name": "Tammy",
+//     "photo": "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+//     "scores": [
+//       1, 4, 3, 4, 2, 5, 1, 2, 4, 1
+//     ]
+//   },
+//   {
+//     "name": "Rebekah",
+//     "photo": "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+//     "scores": [
+//       2, 2, 2, 4, 1, 3, 1, 5, 3, 1
+//     ]
+//   }
+// ]
 
 const rateScores = (userScores, matchScores) => {
   let rating = userScores.reduce((total, currentScore, i) => {
@@ -10,7 +34,23 @@ const rateScores = (userScores, matchScores) => {
   return rating
 }
 
+const compareRates = (userScores, friendsArray) => {
+  let match = friendsArray.reduce((bestMatch, currentMatch) => {
+    let currentRating = rateScores(userScores, currentMatch.scores)
+    let bestRating = bestMatch.rating
+    return currentRating < bestRating ? {rating: currentRating, ...currentMatch} : bestMatch
 
+  }, {
+    rating: 41
+  })
+  return match
+}
 
+module.exports = {
+  compare: compareRates,
+  rate: rateScores
+}
 
-console.log(rateScores(a,b))
+// console.log(compareRates([
+//   5, 4, 5, 4, 1, 2, 1, 2, 3, 1
+// ], friends))
