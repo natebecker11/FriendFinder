50cb83ef9bb779b0bb08ba730e351abbc6519127
natebecker11 <natebecker11@gmail.com>
2018-10-27 10:20:43 -0400

    changed port variable

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const fs = require('fs')
 
 const app = express();
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 const apiRoutes = require('./app/routing/apiRoutes.js')
 const htmlRoutes = require('./app/routing/htmlRoutes.js')
